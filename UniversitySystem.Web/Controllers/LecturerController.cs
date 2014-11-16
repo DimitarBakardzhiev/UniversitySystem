9be1a8b1e8fce493da6cdce415a84be748a66c1b
Rossoner40 <dimitar.bakardzhiev94@gmail.com>
2014-11-16 19:36:51 +0200

    Lecturer profile
    As a teacher you can view your lecturer profile and see your info. An
    easier way to manage your courses. TODO: Edit profile

---
+++ UniversitySystem.Web/Controllers/LecturerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversitySystem.Data;
+
+namespace UniversitySystem.Web.Controllers
+{
+    [Authorize(Roles = "Teacher")]
+    public class LecturerController : Controller
+    {
+        private IUniversitySystemData data;
+
+        public LecturerController()
+            : this(new UniversitySystemData())
+        {
+        }
+
+        public LecturerController(IUniversitySystemData data)
+        {
+            this.data = data;
+        }
+
+        public ActionResult Profile()
+        {
+            var currentUser = this.data.Users.All().FirstOrDefault(u => u.UserName == User.Identity.Name);
+            var currentUserProfile = this.data.Lecturers.All().FirstOrDefault(l => l.User.UserName == currentUser.UserName);
+
+            return View(currentUserProfile);
+        }
+
+        public ActionResult EditProfile()
+        {
+            var currentUser = this.data.Users.All().FirstOrDefault(u => u.UserName == User.Identity.Name);
+            var currentUserProfile = this.data.Lecturers.All().FirstOrDefault(l => l.User.UserName == currentUser.UserName);
+
+            return View(currentUserProfile);
+        }
+
+        public ActionResult Courses()
+        {
+            var currentUser = this.data.Users.All().FirstOrDefault(u => u.UserName == User.Identity.Name);
+            var currentUserProfile = this.data.Lecturers.All().FirstOrDefault(l => l.User.UserName == currentUser.UserName);
+            var courses = currentUserProfile.Courses.AsQueryable();
+
+            return View(courses);
+        }
+    }
+}
